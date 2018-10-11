937eec4c8a2caafc8d002fb64a3ddb5606a2e419
Poncho <poncho.dh@gmail.com>
2018-10-11 18:33:53 +0200

    1

---
+++ p9b/p9b.js
@@ -0,0 +1,7 @@
+var d = 'Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo';
+var l = "Lunes.Martes.Miercoles.Jueves.Viernes.Sabado.Domingo";
+var l = lugares.replace('.',',');
+var d = d.split();
+var l = l.split();
+
+var conciertos = [[d[0],l[0]],[d[1],l[1]],[d[2],l[2]],[d[3],l[3]],[d[4],l[4]],[d[5],l[5]],[d[6],l[6]],[d[7],l[7]]];
