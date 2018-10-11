db112fe15d1a672b744d41836c779dcf56bf7cb2
Poncho <poncho.dh@gmail.com>
2018-10-11 17:53:58 +0200

    finish

---
+++ p6a/p6a.js
@@ -0,0 +1,8 @@
+
+var nombre = "Alfonso"
+var apellido1 = "Sanchez"
+var apellido2 = "Bennai"
+var edad = "29"
+var semana = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+var G = 9,8
+var primeros_3_impares = [1,3,5]
